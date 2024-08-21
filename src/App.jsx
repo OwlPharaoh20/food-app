2be7c73fdb3d7d7f60961fdc080ba14469d41148
GG-Pharaoh <oxdiablo20@gmail.com>
2024-08-21 07:32:39 -0700

    New components added, looking tasty now

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import HeadlineCards from "./components/HeadlineCards"
+import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+
 export default function App( ) {
+
+
   return (
     <div> 
       <Navbar/>
+      <Hero/>
+      <HeadlineCards/>
     </div>
   )
 }
